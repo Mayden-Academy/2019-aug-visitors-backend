32ffcea919e6eb122c678643e406f33b5bc56102
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-12-10 15:13:41 +0000

    added connectToDb function to db service and tested with get request on index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const DbConnector = require('./Services/DbService'); // require db service
 const cors = require('cors');
 const jsonParser = bodyParser.json();
 const app = express();
 const port = 3000;
 app.use(cors())
+
+app.get('/', async function() {
+    let db = await DbConnector() // connect to db
+    let collection = db.collection('visitors')
+    let data = await collection.find().toArray()
+    console.log(data)
+})
 
 
 
