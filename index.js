f7dd2a5c4400417d7f47461399a1522a4730aefb
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-12-10 13:43:30 +0000

    installed cors package for cross application requests and required into index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const jsonParser = bodyParser.json();
 const app = express();
 const port = 3000;
+app.use(cors())
 
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
