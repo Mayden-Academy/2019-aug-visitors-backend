cdc5e54d9080be4dd8827180ea7b379554fbe6f3
georginaw <watson.georgina@gmail.com>
2019-12-12 11:29:54 +0000

    added functionality to store the valid visitor information in the db, catches errors and returns JSON to front end with appropriate success and messages

--- Controllers/index.js
+++ Controllers/index.js
@@ -2,33 +2,32 @@ const validator = require('../Services/ValidationService');
 const visitorsService = require('../Services/VisitorsService');
 const html_specialchars = require('html-specialchars');
 
-function signInVisitor(req, res) {
-
-    req.body.forename = req.body.forename.trim();
-    req.body.surname = req.body.surname.trim();
-    req.body.company = req.body.company.trim();
-
-    if (validator.validateVisitorSignIn(req.body) === true) {
-        let forename =  html_specialchars.escape(req.body.forename);
-        let surname = html_specialchars.escape(req.body.surname);
-        let company = html_specialchars.escape(req.body.company);
-       //call visitor service
-        console.log(surname)
-    } else {
-        //error message to user
-        console.log('boo!')
-    }
-
+async function signInVisitor(req, res) {
     let responseData = {
         'success': false,
         'data': '',
         'msg': 'visitor sign in failed'
     };
 
-    //once stored correctly:
-    responseData.success = true;
-    responseData.msg = 'visitor sign in successful';
+    req.body.forename = req.body.forename.trim();
+    req.body.surname = req.body.surname.trim();
+    req.body.company = req.body.company.trim();
 
+    if (validator.validateVisitorSignIn(req.body) === true) {
+        try {
+            let visitorObject = {
+                'forename': html_specialchars.escape(req.body.forename),
+                'surname': html_specialchars.escape(req.body.surname),
+                'company': html_specialchars.escape(req.body.company)
+            }
+            responseData.success = await visitorsService.storeVisitorInfo(visitorObject);
+            if (responseData.success) {
+                responseData.msg = 'visitor sign in successful';
+            }
+        } catch(e) {
+            responseData.msg = 'unexpected error';
+        }
+    }
     res.json(responseData);
 }
 
